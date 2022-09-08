c773f40ac8bad1f0a8d9434bce0cdec2f32b9d94
SavkoMax <savcomax@gmail.com>
2022-09-08 17:04:19 +0300

    FIX: CI install

--- build_tools/build_tests.sh
+++ build_tools/build_tests.sh
@@ -8,7 +8,7 @@ cd ../../
 python -m venv test_env
 source test_env/bin/activate
 
-python -m cygorithms/cygorithms/dist/*.tar.gz
+python -m cygorithms/cygorithms/.
 python -m pip install pytest
 
 cd cygorithms/cygorithms
