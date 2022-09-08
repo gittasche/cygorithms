26c27ac4033a70aa12f21ad86584f688a77f27fb
SavkoMax <savcomax@gmail.com>
2022-09-08 16:39:34 +0300

    FIX: CI update

--- build_tools/build_tests.sh
+++ build_tools/build_tests.sh
@@ -8,8 +8,8 @@ cd ../../
 python -m venv test_env
 source test_env/bin/activate
 
-python -m pip install .
 python -m pip install pytest
 
 cd cygorithms/cygorithms
+python -m pip install .
 python -m pytest /tests
